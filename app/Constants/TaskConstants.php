34f2850e01d2bb0e12984882f277a500c9740069
adib_syarif <asyarif644@gmail.com>
2024-08-01 15:40:51 +0700

    refactor (task): added search for task

--- app/Constants/TaskConstants.php
+++ app/Constants/TaskConstants.php
@@ -14,4 +14,6 @@ class TaskConstants
     // Error messages
     public const INTERNAL_SERVER_ERROR = 'Internal Server Error';
     public const NOT_UPDATE = 'No changes detected';
+    public const NO_CONTENT = 'No content detected';
+    
 }
